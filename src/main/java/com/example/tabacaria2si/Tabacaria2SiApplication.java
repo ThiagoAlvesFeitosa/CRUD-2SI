fa1f9200bbd9559789235d57ba7e584b124858de
Thiago Alves Feitosa <thiago.feitosa.alves@hotmail.com>
2024-03-27 16:48:18 -0300

    Initial commit

---
+++ src/main/java/com/example/tabacaria2si/Tabacaria2SiApplication.java
@@ -0,0 +1,13 @@
+package com.example.tabacaria2si;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tabacaria2SiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Tabacaria2SiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/tabacaria2si/Tabacaria2SiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tabacaria2si;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Tabacaria2SiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
